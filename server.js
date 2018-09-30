e77b371da61c7975b49e974effad68e6ff3ee863
João Salgado <jgs1884@gmail.com>
2018-09-30 18:54:12 -0300

    fix server problems

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(compression());
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/app-products'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/dist/app-products/index.html');
 });
 
 app.listen(process.env.PORT || 5000);
